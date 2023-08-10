8e88eef815e45d40fb05855e560ea1d52d1e4b70
AbdulWahab <kwahab214@gmail.com>
2023-08-11 01:42:47 +0500

    Added routes for code and image generator as well as their UI code

---
+++ app/api/image/route.ts
@@ -0,0 +1,57 @@
+import { Configuration, OpenAIApi } from "openai";
+
+import { auth } from "@clerk/nextjs";
+
+import { NextResponse } from "next/server";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function POST(req: Request) {
+  console.log("Conversation Request: ", req);
+
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = 1, resolution = "512x512" } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!configuration.apiKey) {
+      return new NextResponse("OpenAi API key Not configured", { status: 500 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("prompt is required.", { status: 400 });
+    }
+
+    if (!amount) {
+      return new NextResponse("amount is required.", { status: 400 });
+    }
+
+    if (!resolution) {
+      return new NextResponse("resolution is required.", { status: 400 });
+    }
+
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
+
+    return NextResponse.json(response.data.data);
+  } catch (error) {
+    console.log("Image Error: ", error);
+    return new NextResponse(
+      "Sorry, I'm having trouble understanding you. Please try again.",
+      {
+        status: 500,
+      }
+    );
+  }
+}
