deee50d64cddcd2eab9bab36db19829c106be2d4
AbdulWahab <kwahab214@gmail.com>
2023-08-12 15:41:10 +0500

    Mobile progress bar updated

--- components/MobileSIdebar.tsx
+++ components/MobileSIdebar.tsx
@@ -1,41 +1,43 @@
-'use client'
-import { GiHamburgerMenu } from 'react-icons/gi';
+"use client";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
-import Sidebar from './Sidebar';
-import { useState, useEffect } from 'react';
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./Sidebar";
+import { useState, useEffect } from "react";
 
-const MobileSideBar = () => {
-    const [isMounted, setIsMounted] = useState(false)
+interface MobileSideBarProps {
+  apiLimitCount: number;
+}
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
+const MobileSideBar = ({ apiLimitCount = 0 }: MobileSideBarProps) => {
+  const [isMounted, setIsMounted] = useState(false);
 
-    if (!isMounted) {
-        return null
-    }
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
+  if (!isMounted) {
+    return null;
+  }
 
-
-    return (
-        <Sheet>
-            <SheetTrigger>
-                <Button
-                    className="
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button
+          className="
                 md:hidden
                 "
-                    variant='ghost'
-                    size='icon'
-                >
-                    <GiHamburgerMenu className="w-6 h-6" />
-                </Button>
-            </SheetTrigger>
-            <SheetContent side='left' className="p-0">
-                <Sidebar />
-            </SheetContent>
-        </Sheet>
-    );
-}
+          variant="ghost"
+          size="icon"
+        >
+          <GiHamburgerMenu className="w-6 h-6" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar apiLimitCount={apiLimitCount} />
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 export default MobileSideBar;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,21 @@
-
 import { UserButton } from "@clerk/nextjs";
 
 import MobileSideBar from "./MobileSIdebar";
-const Navbar = () => {
-    return (
-        <div className="flex items-center p-4">
+import { getApiLimitCount } from "@/lib/api-limit";
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+  return (
+    <div className="flex items-center p-4">
+      <MobileSideBar apiLimitCount={apiLimitCount} />
 
-            <MobileSideBar />
-
-
-            <div className="flex
-            z w-full justify-end">
-                <UserButton afterSignOutUrl='/' />
-            </div>
-        </div>
-
-    );
-
-
-}
-
+      <div
+        className="flex
+            z w-full justify-end"
+      >
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
